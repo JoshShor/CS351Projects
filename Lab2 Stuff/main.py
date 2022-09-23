1bbf4b84ad24e07d2f0007b6da149686247249f5
JTS <jtshor@gmail.com>
2022-09-23 00:14:15 -0700

    done

---
+++ Lab2 Stuff/main.py
@@ -0,0 +1,88 @@
+import re
+
+
+def has_integer(txtstr):
+    intstuff = '^[-+]?\d+$'
+    tester = re.search(intstuff, txtstr)
+
+    if tester:
+        print("Integer exists here in " + txtstr)
+    else:
+        print("no integer in " + txtstr)
+
+
+def has_float(txtstr):
+    floatstuff = '[+-]?([0-9]*[.])[0-9]+'  # float (consists of 1 or more digits before and after decimal point)
+    tester = re.search(floatstuff, txtstr)
+
+    if tester:
+        print("float exists here in " + txtstr)
+    else:
+        print("no floating numbers in " + txtstr)
+
+
+# A float with exactly 2 digits after the decimal point
+
+def has_float_two_num(txtstr):
+    float_two_num = '[+-]?([0-9]*[.])[0-9][0-9]$'
+    tester = re.search(float_two_num, txtstr)
+
+    if tester:
+        print("∃ float with 2 digits after decimal exists here in " + txtstr)
+    else:
+        print("no float with 2 digital after decimal here in " + txtstr)
+
+def has_floatwithendf(txtstr):
+    floatstuff = '[+-]?([0-9]*[.])[0-9]+[f]'
+    tester = re.search(floatstuff, txtstr)
+
+    if tester:
+        print("∃ float that ends in f " + txtstr)
+    else:
+        print("no float that ends in f here in " + txtstr)
+
+def has_cap(txtstr):
+    c_stringer = '[A-Z]+[a-z]+$|[A-Z]+[a-z]+[0-9]+$'
+    tester = re.search(c_stringer, txtstr)
+
+    if tester:
+        print("Capital Letter + Lowercase + maybe digits here in " + txtstr)
+    else:
+        print("No Capital Letter + Lowercase + maybe digits here in " + txtstr)
+
+
+def has_tripdigitsandlets(txtstr):
+    triple_digi_plus_letter = '[0-9][0-9][0-9][a-z][a-z]+'
+    tester = re.search(triple_digi_plus_letter, txtstr, re.IGNORECASE)
+
+    if tester:
+        print("3 digits + 2+ letters here in " + txtstr)
+    else:
+        print("No 3 digits + 2+ letters here in " + txtstr)
+
+
+teststring = ["22.11", "23", "66.7f", "123abcde", "Case44", "Happy", "78", "66.7", "yes123", "Book111"]
+
+for i in teststring:
+    has_integer(i)
+    has_float(i)
+    has_float_two_num(i)
+    has_floatwithendf(i)
+    has_cap(i)
+    has_tripdigitsandlets(i)
+    print('\n')
+
+str1 = "22 street"
+str2 = "90years"
+def removeint(str):
+    print("string is " + str)
+    new_str = ""
+    for i in str.split():
+        if i.isdigit():
+            print("found integer " + i + " at beginning of this string")
+            new_str = str.replace(i, "")
+    print("string is now " + new_str)
+
+
+removeint(str1)
+removeint(str2)
